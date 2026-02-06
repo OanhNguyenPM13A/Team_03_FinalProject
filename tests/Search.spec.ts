b9d13c64093c631aab07d2154e7feb1dec7a0c4b
NGUYEN THI KIEU OANH <oanhnguyen@Oanhs-Macbook-Air.local>
2026-02-06 10:09:36 +0700

    test case 09 module search and booking

--- tests/Search.spec.ts
+++ tests/Search.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../page/HomePage';
+import { Helper } from '../utils/helper';
 
 test.describe('Search and Booking Module ====>', () => {
     test('TC08: Search for a location : Hồ Chí Minh', async ({ page }) => {
@@ -34,36 +35,62 @@ test.describe('Search and Booking Module ====>', () => {
 
     });
 
+    // TODO: Implementation for dynamic date, 
+    // cases: previous date of current date, next years, pick  only one day, 2 different months, etc., 
     test('TC09: Search for check-in/check-out', async ({ page }) => {
+        // Initialize HomePage object
+        const homePage = new HomePage(page);
+
+        // Step1: Access website
+        await homePage.goto();
+
+        // Step2: Select check-in/check-out date
+        await homePage.selectCheckInOutDate('07/02/2026', '14/02/2026');
+
+        const expected = Helper.SearchModule.expectCheckInOutDisplayed('07/02/2026', '14/02/2026');
+
+        await expect(
+            page.locator('p', { hasText: expected })
+        ).toBeVisible();
+
+        // Step3: Click Search Icon Button
+        await homePage.clickSearchIconButton();
+
+        const resultSummary = page.locator('p', {
+            hasText: 'chỗ ở tại'
+        });
+
+        await expect(resultSummary).toContainText('07/02/2026');
+        await expect(resultSummary).toContainText('14/02/2026');
 
     });
-    
-    test('TC10: Search for number of guests', async ({ page }) => {
 
-    });
+    // test('TC10: Search for number of guests', async ({ page }) => {
+
+    // });
 
     // TC11: Filter theo khoảng giá
-    test('TC11: Filter by price range', async ({ page }) => {
+    // test('TC11: Filter by price range', async ({ page }) => {
 
-    });
+    // });
 
     // TC12: Xem chi tiết phòng
-    test('TC12: View room details', async ({ page }) => {
+    // test('TC12: View room details', async ({ page }) => {
 
-    });
+    // });
 
     // TC13: Kiểm tra thông tin phòng hiển thị đầy đủ
-    test('TC13: Verify complete room information display', async ({ page }) => {
+    // test('TC13: Verify complete room information display', async ({ page }) => {
 
-    });
+    // });
 
     // TC14: Test chức năng pagination
-    test('TC14: Test pagination functionality', async ({ page }) => {
-        
-    });
+    // test('TC14: Test pagination functionality', async ({ page }) => {
+
+    // });
 
     // TC15: Sort kết quả theo giá/đánh giá
-    test('TC15: Sort results by price/rating', async ({ page }) => {
+    // test('TC15: Sort results by price/rating', async ({ page }) => {
 
-    });
+    // });
 });
